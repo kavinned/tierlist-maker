561d7fc308c2fc7f80b4ea1d5c728c6b8af9f1f2
kavin <kavin.ned@gmail.com>
2025-02-10 21:58:59 +0800

    Refactor TierHeader component styles for improved button visibility and hover effects, and enhance global styles for consistency.

--- src/app/(components)/TierHeader.jsx
+++ src/app/(components)/TierHeader.jsx
@@ -13,16 +13,18 @@ export default function TierHeader({
             style={{ backgroundColor: items[tier].color }}
         >
             <p>{items[tier].name}</p>
-            <div className="flex md:flex-row flex-col gap-2 absolute md:bottom-3 md:justify-evenly justify-end h-full md:h-fit">
+            <div className="flex absolute bottom-3 h-fit rounded-full overflow-hidden">
                 <button className="tier-header-button">
                     <Edit2Icon
                         className="fill-sky-500 hover:scale-125 hover:fill-sky-700 transition-all duration-200 stroke-black"
+                        size={15}
                         onClick={() => handleEditTier(tier)}
                     />
                 </button>
                 <button className="tier-header-button">
                     <Trash
                         className="fill-red-500 hover:scale-125 hover:fill-red-700 transition-all duration-200 stroke-black"
+                        size={15}
                         onClick={() => handleDeleteTier(tier)}
                     />
                 </button>
