8f1972d646d95fbf6430a0628816794f9156d8e5
kavin <kavin.ned@gmail.com>
2025-02-11 01:48:50 +0800

    add tooltip for copy link

--- src/app/(components)/CopyLink.jsx
+++ src/app/(components)/CopyLink.jsx
@@ -3,14 +3,14 @@ import { ClipboardCopyIcon } from "lucide-react";
 
 export default function CopyLink({ handleCopyToClipboard }) {
     return (
-        <p className="fixed bottom-4 right-4 text-sm flex text-center mt-3 bg-[#323638] rounded-md p-2 border border-white items-center justify-cente w-fit opacity-70 hover:opacity-100 transition-all duration-150 ease-in-out">
-            <Share2 className="w-4 h-4 stroke-white fill-blue-400" />
-            <span
-                className="ml-2 cursor-pointer border-rid border p-1 rounded bg-[rgba(255,255,255,0.2)] hover:bg-[rgba(255,255,255,0.6)] focus:bg-[rgba(255,255,255,0.6)] transition-all duration-100 ease-linear"
-                onClick={handleCopyToClipboard}
-            >
-                <ClipboardCopyIcon className="w-4 h-4 text-gray-900" />
-            </span>
-        </p>
+        <div className="fixed bottom-4 right-4 text-sm flex text-center mt-3 bg-[#323638] rounded-md p-2 border border-white items-center justify-center w-[5rem] opacity-70 hover:opacity-100 transition-all duration-150 ease-in-out">
+            <div className="copy-button-container">
+                <Share2 className="w-4 h-4 stroke-white fill-blue-400" />
+                <span className="copy-button" onClick={handleCopyToClipboard}>
+                    <ClipboardCopyIcon className="w-4 h-4 text-gray-900" />
+                    <div className="tooltip">Copy link</div>
+                </span>
+            </div>
+        </div>
     );
 }
